3ace056fb7a620d20332ab6e8ffcedafd28ad03d
jorge <jorge@debian>
2017-07-21 17:14:21 +0800

    user session

--- multichat/chat/consumers.py
+++ multichat/chat/consumers.py
@@ -1,6 +1,9 @@
+import json
+
 from channels.auth import channel_session_user_from_http
 
 from .models import Room
+from channels import Channel
 
 # This decorator copies the user from the HTTP session (only available in
 # websocket.connect or http.request messages) to the channel session (available
@@ -21,3 +24,17 @@ def ws_disconnect(message):
             room.websocket_group.discard(message.reply_channel)
         except Room.DoesNotExist:
             pass
+
+# Unpacks the JSON in the received WebSocket frame and puts it onto a channel
+# of its own with a few attributes extra so we can route it
+# This doesn't need @channel_session_user as the next consumer will have that,
+# and we preserve message.reply_channel (which that's based on)
+def ws_receive(message):
+    # All WebSocket frames have either a text or binary payload; we decode the
+    # text part here assuming it's JSON.
+    # You could easily build up a basic framework that did this encoding/decoding
+    # for you as well as handling common errors.
+    payload = json.loads(message['text'])
+    payload['reply_channel'] = message.message.content['reply_channel']
+    Channel("chat.receive").send(payload)
+
